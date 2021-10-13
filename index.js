ed0c7ee5db5502aed51813f099d7e6e33215373f
Edgar Allan Glez <edgarallanglez@gmail.com>
2021-10-13 12:49:34 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+
+var knex = require('knex');
+const imageThumbnail = require('image-thumbnail');
+
+exports.triggerETL = (req, res) => {
+  const limit = 1000;
+
+  const db = knex({
+    client: 'mssql',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      options: {
+        enableArithAbort: true,
+      }
+    },
+    useNullAsDefault: true 
+  });
+
+  const photos = await db.select('RowId', 'File') 
+    .from('dbo.photo as PH')
+    .where('PH.ThumbFile', '=', 'NULL')
+    .orderBy('PH.RowId', 'asc')
+    .limit(limit);
+
+  let options = { percentage: 66, jpegOptions: { force: true, quality: 33 } };
+  for (const photo of photos) {    
+    const ThumbFile = await imageThumbnail(photo.File, options);
+    const body = { ThumbFile }
+
+    await db('dbo.photo')
+    .update(body)
+    .where('RowId', photo.RowId);
+  }
+
+  res.status(200).send("success");
+};
